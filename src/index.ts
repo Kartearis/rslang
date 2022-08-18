b7d023f5f43bf53b94a1d86c7fb18c974c1bee1b
Matveev Timofey <justshowel@gmail.com>
2022-08-18 19:04:55 +0300

    init: Create basic ts project

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+
+import './global.css';
+
+// Empty js template
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/index.ts'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /(?<!\.local)\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.local\.css$/i,
+                use: ['css-loader'],
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.html?$/,
+                use: 'html-loader',
+            },
+            {
+                test: /\.(jpg|png|webp)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                },
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.ts'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, '../news-api-dist'),
+        hashFunction: 'xxhash64',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
