045755f3de35c38f7d3cd90a3f1ebdbc515dbc37
Matveev Timofey <justshowel@gmail.com>
2022-08-19 16:34:33 +0300

    feat: Implement assetDefined

---
+++ src/helpers/helpers.ts
@@ -0,0 +1,6 @@
+
+export function assertDefined<Type>(value: Type): NonNullable<Type> {
+    if (value === undefined || value === null)
+        throw new Error('Asserted value is not defined!');
+    return value as NonNullable<Type>;
+}
