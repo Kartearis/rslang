e9ff7342724a453edfecc912ff1ffb8061d820fa
Matveev Timofey <justshowel@gmail.com>
2022-08-19 16:35:52 +0300

    feat: Implement Router and wire it to appController

---
+++ src/controllers/appController.ts
@@ -0,0 +1,16 @@
+import RouterController from "./routerController";
+import {assertDefined} from "../helpers/helpers";
+
+
+export default class AppController {
+    constructor() {
+        const router: RouterController = RouterController.getInstance();
+        const viewContainer: HTMLElement = assertDefined(document.querySelector('.content'));
+        router.setRootElement(viewContainer);
+        router.reOpenCurrent();
+        // tmp
+        assertDefined(document.querySelector('#garage')).addEventListener('click', () => router.navigate('/'));
+        assertDefined(document.querySelector('#records')).addEventListener('click', () => router.navigate('/test'));
+
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 
 import './global.css';
+import AppController from "./controllers/appController";
 
-// Empty js template
+const app = new AppController();
