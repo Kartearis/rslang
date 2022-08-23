b14e11debfe6088c33440e5b906c3fd363610627
Evg2ChainzDev <88166547+Evg2ChainzDev@users.noreply.github.com>
2022-08-23 09:05:26 +0300

    feat: main done

--- src/views/mainView.ts
+++ src/views/mainView.ts
@@ -6,6 +6,8 @@ export default class MainView extends ViewInterface {
     // }
 
     show(): void {
-        this.rootElement.innerText = 'Hello view';
+        this.rootElement.innerHTML = mainBody;
     }
 }
+
+const mainBody = `<h1>RSLANG APP</h1><p>Take advantage of our bite-sized lessons so you can study at a time that's best for you. You'll only see exercises at the right level for you and can achieve certificates when you pass each level.</p>`;
