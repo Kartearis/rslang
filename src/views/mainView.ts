47f8a09dfa7790273e789ebae18782beada9a41a
Evg2ChainzDev <88166547+Evg2ChainzDev@users.noreply.github.com>
2022-08-24 18:07:31 +0300

    feat: main done

--- src/views/mainView.ts
+++ src/views/mainView.ts
@@ -1,4 +1,14 @@
 import ViewInterface from './viewInterface';
+import img1 from '../assets/imgs/1.jpg';
+import img2 from '../assets/imgs/2.jpg';
+import img3 from '../assets/imgs/3.jpg';
+import img4 from '../assets/imgs/4.jpg';
+import img5 from '../assets/imgs/5.jpg';
+import img6 from '../assets/imgs/6.jpg';
+
+import imgDev1 from '../assets/imgs/dev1.png';
+import imgDev2 from '../assets/imgs/dev2.png';
+import imgDev3 from '../assets/imgs/dev3.jpg';
 
 export default class MainView extends ViewInterface {
     // constructor(rootElement: HTMLElement) {
@@ -10,4 +20,33 @@ export default class MainView extends ViewInterface {
     }
 }
 
-const mainBody = `<h1>RSLANG APP</h1><p>Take advantage of our bite-sized lessons so you can study at a time that's best for you. You'll only see exercises at the right level for you and can achieve certificates when you pass each level.</p>`;
+const mainBody = `<div class="main-wrapper"><h1 class="main-title">RSLANG APP</h1>
+                    <div class="advantage-block">
+                        <div class="advantage-block__item"><img class="advantage-block__img" height="200" width="100%" src="${img1}"><p class="advantage-block__text">Покажем куда расти</<p></div>
+                        <div class="advantage-block__item"><img class="advantage-block__img" height="200" width="100%" src="${img2}"><p class="advantage-block__text">Всего 15 минут в день</<p></div>
+                        <div class="advantage-block__item"><img class="advantage-block__img" height="200" width="100%" src="${img3}"><p class="advantage-block__text">Обучение в игровой форме, скучно не будет!</<p></div>
+                        <div class="advantage-block__item"><img class="advantage-block__img" height="200" width="100%" src="${img4}"><p class="advantage-block__text">Осторожно! Вызывает привыкание :) </<p></div>
+                        <div class="advantage-block__item"><img class="advantage-block__img" height="200" width="100%" src="${img5}"><p class="advantage-block__text">Геймификация доказала свою эффективность для всех возрастов.</<p></div>
+                        <div class="advantage-block__item"><img class="advantage-block__img" height="200" width="100%" src="${img6}"><p class="advantage-block__text">Наша миссия - сделать изучение языков доступным для всех.</<p></div>
+                    </div>
+                    <H2 class="our-team-title">НАША КОМАНДА</H2>
+                    <div class="team-block">
+                        <div class="dev-card">
+                            <img class="dev-card__img" src="${imgDev1}" width="50">
+                            <a href="https://github.com/Kartearis">Kartearis</a>
+                            <p>Игра1, игра2</p>
+                        </div>
+                                                <div class="dev-card">
+                            <img class="dev-card__img" src="${imgDev2}" width="50">
+                            <a href="https://github.com/Kartearis">Palasja</a>
+                            <p>Игра1, игра2</p>
+                        </div>
+                                                <div class="dev-card">
+                            <img class="dev-card__img" src="${imgDev3}" width="50">
+                            <a href="https://github.com/Kartearis">Evg2Chainz</a>
+                            <p>Игра1, игра2</p>
+                        </div>
+                        
+                    </div>
+
+</div>`;
