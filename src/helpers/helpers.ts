362110be2162588953b3a62619239720c0ea6aca
Matveev Timofey <justshowel@gmail.com>
2022-09-04 03:38:55 +0300

    feat: Implement typed Object.entries and Object.keys (as helpers)

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -7,6 +7,22 @@ export function getHostPath(path: string) {
     return `${HOST}\\${path}`;
 }
 
+export function formatDate(date: Date): string {
+    return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2,'0')}`
+}
+
+export function scanDate(date: string): Date {
+    return new Date(date);
+}
+
+export function typedKeys<Type>(obj: Type): (keyof Type)[] {
+    return Object.keys(obj) as (keyof Type)[];
+}
+
+export function typedEntries<Type>(obj: Type): [keyof Type, Type[keyof Type]][] {
+    return Object.entries(obj) as [keyof Type, Type[keyof Type]][];
+}
+
 export const HOST = 'https://rs-lang-proj.herokuapp.com';
 export const TOKEN_NAME = 'jwt';
 export const REFRESH_TOKEN_NAME = 'refreshToken';
