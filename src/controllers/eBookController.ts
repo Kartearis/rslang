7418cb1178d3926214bcf13037a28ad801e169d8
palasja <palasja@gmail.com>
2022-09-04 18:37:31 +0300

    refactor: add getAllWords to eBoocComtroller

--- src/controllers/eBookController.ts
+++ src/controllers/eBookController.ts
@@ -137,6 +137,32 @@ class EBookController {
             throw Error('Access token is missing or invalid');
         }
     }
+    async getAllWordsUser(): Promise<wordType[]> {
+        const { userId, jwt } = localStorage;
+        //3600 words in base
+        const MAX_WORDS = 3600;
+        const response = await fetch(
+            `${HOST}/users/${userId}/aggregatedWords?wordsPerPage=${MAX_WORDS}`,
+            {
+                signal: this.abortController.signal,
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    Authorization: `Bearer ${jwt}`,
+                },
+            }
+        );
+        if (response.status === 200) {
+            const arr = (await response.json()) as responceUserWords;
+            const arrWords: wordType[] = arr[0].paginatedResults;
+            arrWords.forEach((word) => {
+                if (word._id !== undefined) word.id = word._id;
+            });
+            return arrWords;
+        } else {
+            throw Error('Access token is missing or invalid');
+        }
+    }
     updateWord(wordId: string, property: wordProperty) {
         let flgSuccess = false;
         for (let i = 0; i < this.groupWords.words.length; i++) {
