796fc4e22ae62b1466d34858a55dfea75c25fa6f
palasja <palasja@gmail.com>
2022-09-04 23:20:55 +0300

    add disable submit btn auth while data invalidate

--- src/views/authorization/signinView.ts
+++ src/views/authorization/signinView.ts
@@ -1,3 +1,4 @@
+import LoadingOverlay from '../../components/loadingOverlay';
 import UserController from '../../controllers/userController';
 import { assertDefined } from '../../helpers/helpers';
 import Authorization from './authorization';
@@ -13,9 +14,11 @@ class SigninView extends Authorization {
         const form = this.getAuthForm();
         form.classList.add('signin-form');
         this.fillLoginForm(form);
-        form.addEventListener('input', () =>
-            assertDefined(form.querySelector<HTMLParagraphElement>('#errMesage')).classList.add('hidden')
-        );
+        form.addEventListener('input', (ev) => {
+            // const target = ev.target as HTMLSelectElement;
+            // console.log(target.checkValidity());
+            assertDefined(form.querySelector<HTMLParagraphElement>('#errMesage')).classList.add('hidden');
+        });
         this.rootElement.innerText = '';
         const formContainer = document.createElement('div');
         formContainer.classList.add('auth-container');
@@ -35,9 +38,15 @@ class SigninView extends Authorization {
         form.append(submit);
     }
     private signinAction() {
+        // const loadingOverlay = new LoadingOverlay(false).show();
+        // try {
+        //     this.rootElement.append(loadingOverlay);
         const email = assertDefined(document.querySelector<HTMLInputElement>('#email')).value;
         const password = assertDefined(document.querySelector<HTMLInputElement>('#password')).value;
         UserController.getInstance().signIn(email, password);
+        // } finally {
+        //     loadingOverlay.hide();
+        // }
     }
 }
 
